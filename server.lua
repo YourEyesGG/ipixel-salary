94f2a17456319cd4debcbc30944bcfebe8301b6b
YourEyes <72436392+YourEyesGG@users.noreply.github.com>
2021-06-07 09:13:05 +0700

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,28 +1,11 @@
 ESX = nil
-local totalsalary = 0
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterNetEvent('ipixel-salary:takes')
-AddEventHandler('ipixel-salary:takes', function(source)
-    if totalsalary[source] > 1 then
-        xPlayera = ESX.GetPlayerFromId(source)
-        xPlayera.addMoney(totalsalary[source])
-        totalsalary[source] = 0
-        TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = 'Walet + $' .. totalsalary .. '.' })
-    else
-        TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Your salary is not enough.' })
-    end
-end)
-
-RegisterNetEvent('ipixel-salary:salary')
-AddEventHandler('ipixel-salary:salary', function(source)
-	xPlayer = ESX.GetPlayerFromId(source)
-
-	if totalsalary[source] <= 1500 then
-		totalsalary[source] = totalsalary[source] + salary
-		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = 'Salary + ' .. salary })
-	else
-		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Your Salary is Full, Take it out in near ATM.' })
-	end
+AddEventHandler('ipixel-salary:takes', function(source, salary)
+    	xPlayer = ESX.GetPlayerFromId(source)
+    	xPlayer.addMoney(salary)
+   	TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = 'Walet + $' .. salary .. '.' })
+	TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Salary - $' .. salary .. '.' })
 end)
