2797e3ba14be3088f0a3c7856e252c52f7c95b74
YourEyes <72436392+YourEyesGG@users.noreply.github.com>
2021-06-09 07:11:45 +0700

    Updated with SQL

--- server.lua
+++ server.lua
@@ -2,19 +2,6 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-RegisterNetEvent('ipixel-salary:takes')
-AddEventHandler('ipixel-salary:takes', function(source, salary, ehm)
-	if ehm == 1 then
-    		xPlayer = ESX.GetPlayerFromId(source)
-    		xPlayer.addMoney(salary)
-   		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = 'Walet + $' .. salary .. '.' })
-		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Salary - $' .. salary .. '.' })
-	else
-		TriggerClientEvent('ipixel-drugs:AntiExploit', source, 'You`re attempt to Exploit iPixel Salary System')
-		webhok(source)
-	end
-end)
-
 function webhok(source)
 	hook = Config.Webhook
 	identifiers = ExtractIdentifiers(source)
@@ -55,3 +42,65 @@ function ExtractIdentifiers(src)
 
     return identifiers
 end
+
+-- Take salary
+RegisterNetEvent('ipixel-salary:takes')
+AddEventHandler('ipixel-salary:takes', function(total, ah)
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+	
+	if ah == 3 then
+	    if total > 0 then
+		xPlayer.addMoney(total)
+		-- Reset salary to 0
+		MySQL.Sync.fetchAll('UPDATE users SET salary = 0 WHERE identifier = @identifier', {
+		    ['@identifier'] = xPlayer.identifier
+		})
+		-- Refresh salary amount
+		TriggerEvent('ipixel-salary:getSalary', _source)
+		TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'success', text = 'Walet + $' .. total .. '' })
+		TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'success', text = 'Salary - $' .. total .. '' })
+	    else
+		TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'error', text = 'You have not enough salary !' })
+	    end
+	else
+		TriggerClientEvent('ipixel-drugs:AntiExploit', source, 'You`re attempt to Exploit iPixel Salary System')
+		webhok(source)
+	end
+end)
+
+
+-- Get user salary
+RegisterNetEvent('ipixel-salary:getSalary')
+AddEventHandler('ipixel-salary:getSalary', function(source)
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+    local salary = MySQL.Sync.fetchAll('SELECT salary FROM users WHERE identifier = @identifier', {
+        ['@identifier'] = xPlayer.identifier
+    })
+    TriggerClientEvent('ipixel-salary:currentSalary', _source, salary[1].salary)
+end)
+
+-- Update user salary
+RegisterNetEvent('ipixel-salary:updateSalary')
+AddEventHandler('ipixel-salary:updateSalary', function(amount)
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+    
+    MySQL.Sync.fetchAll('UPDATE users SET salary = @salary WHERE identifier = @identifier', {
+        ['@identifier'] = xPlayer.identifier,
+        ['@salary'] = amount
+    })
+end)
+
+-- Get job salary amount
+RegisterNetEvent('ipixel-salary:getSalaryAmount')
+AddEventHandler('ipixel-salary:getSalaryAmount', function()
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+
+    local salaryamount = MySQL.Sync.fetchAll('SELECT salary FROM job_grades WHERE name = @jobname', {
+        ['@jobname'] = xPlayer.job.grade_name
+    })
+    TriggerClientEvent('ipixel-salary:salaryAmount', _source, salaryamount[1].salary)
+end)
